e3afbfdd154cbfca973341d6bcc5b8bb63a3f86d
muhammedsubhan <muhammedsubhan692@gmail.com>
2024-01-30 19:32:29 +0500

    permission for image on Iphone --feature

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,8 +1,36 @@
-import { launchCameraAsync } from "expo-image-picker";
-import { Button, View } from "react-native";
+import {
+  PermissionStatus,
+  launchCameraAsync,
+  useCameraPermissions,
+} from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
 
 const ImagePicker = () => {
+  const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+
+  const verifyPermission = async () => {
+    if (cameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (cameraPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "you need to grant permissions to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  };
+
   const takeImageHandler = async () => {
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
