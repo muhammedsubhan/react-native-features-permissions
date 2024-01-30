8778119237dd8510c24ebe8c961dede6593f0908
muhammedsubhan <muhammedsubhan692@gmail.com>
2024-01-30 18:36:34 +0500

    clicking photos on android --feature

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,11 +1,50 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnterTitle] = useState("");
+
+  const changeHandlerTitle = (enteredText) => {
+    setEnterTitle(enteredText);
+  };
   return (
-    <View>
-      <Text>The Place Form</Text>
-    </View>
+    <>
+      <ScrollView style={Style.form}>
+        <View>
+          <Text style={Style.label}>Title</Text>
+          <TextInput
+            style={Style.input}
+            onChangeText={changeHandlerTitle}
+            value={enteredTitle}
+          />
+        </View>
+        <ImagePicker />
+      </ScrollView>
+    </>
   );
 };
 
 export default PlaceForm;
+
+const Style = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
