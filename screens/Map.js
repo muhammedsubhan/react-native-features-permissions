18540ec5b090ba9ee3095f3f0428529621c45538
muhammedsubhan <muhammedsubhan692@gmail.com>
2024-01-31 05:46:34 +0500

    Location Marker --feature

--- screens/Map.js
+++ screens/Map.js
@@ -1,6 +1,10 @@
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+
 const Map = () => {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -8,13 +12,36 @@ const Map = () => {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
-};
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
 
-export default Map;
+    setSelectedLocation({ lat: lat, lng: lng });
+  };
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Pick Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
+};
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
 });
+
+export default Map;
